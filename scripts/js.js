c2d83dd0621408a9ab31a917ce649e04373e4ef0
JoseIsaac6858 <70914406+JoseIsaac6858@users.noreply.github.com>
2020-09-19 17:54:26 -0500

    Add files via upload

---
+++ scripts/js.js
@@ -0,0 +1,7 @@
+function hola(){
+	alert("registrate pon tu nombre" + iname)
+};
+let iname = "hola";
+function MasInfo(){
+    alert("hola" + " esta pagina aun esta en desarrollo por favor agradecemos su comprencion, si te interesa ayudarnos ya sea con el factor visual de la pagina o sabes algo de programacion web contactame en cualquiera de los medios que estan en el pie de pagina ¨estoy mas activo en instagram¨ actualmento solo estoy yo trabajando en este proyecto no olvides suscribirte a mi canal de youtube tutoriales jose")
+}
